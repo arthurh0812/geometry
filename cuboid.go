7a708befc5232b8f5d22b6c371639e71a31f5fde
arthurh0812 <arthurh1204@gmail.com>
2021-04-12 11:19:06 +0200

    Cuboid constructor function implemented + write methods added

--- cuboid.go
+++ cuboid.go
@@ -6,16 +6,36 @@ type Cuboid struct {
 	depth float64
 }
 
+func NewCuboid(height, width, depth float64) Cuboid {
+	return Cuboid{
+		height: height,
+		width: width,
+		depth: depth,
+	}
+}
+
 func (c *Cuboid) Height() float64 {
 	return c.height
+}
+
+func (c *Cuboid) SetHeight(h float64) {
+	c.height = h
 }
 
 func (c *Cuboid) Width() float64 {
 	return c.width
 }
 
+func (c *Cuboid) SetWidth(w float64) {
+	c.width = w
+}
+
 func (c *Cuboid) Depth() float64 {
 	return c.depth
+}
+
+func (c *Cuboid) SetDepth(d float64) {
+	c.depth = d
 }
 
 func (c *Cuboid) Volume() float64 {
